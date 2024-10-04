3533ec4d2cf8e95df01d222b6055c431acd90cdf
Kamilla2424 <mohamedkamilla6@gmail.com>
2024-10-04 11:46:03 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const { postUser, getUser } = require('./controllers/user-controller.js')
+
+const app = express()
+app.use(express.json())
+
+app.get('/api/healthCheck', (req,res)=>{
+    res.status(200).send({msg:"server live"})
+})
+
+app.listen(9090, () => {
+    console.log('Server started on port 9090');
+})
