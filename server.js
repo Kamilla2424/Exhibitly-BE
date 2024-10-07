fb2b56c6f13c11fbf7acbbe0fa15bd5d7d000e7d
Kamilla2424 <mohamedkamilla6@gmail.com>
2024-10-07 11:15:38 +0100

    allow cross origin resource sharing

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const { postUser, getUser } = require('./controllers/user-controller.js')
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.get('/api/healthCheck', (req,res)=>{
     res.status(200).send({msg:"server live"})
